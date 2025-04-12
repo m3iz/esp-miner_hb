b064cec7dcc0aedce34584853776007f32d440f3
Timur Mingaliev <timur@mingaliev.com>
2025-04-12 16:41:13 +0300

    Ukroshaizing

--- piaxe/miner.py
+++ piaxe/miner.py
@@ -596,13 +596,14 @@ class BM1366Miner:
                         self.stats.hashing_speed = self.hash_rate()
 
                         current_time = time.time()
-                        logging.warning(f"{colors.OKGREEN}hash rate:  {colors.ENDC}")
-                        logging.warning(f"{colors.OKGREEN}60s: {colors.FAIL}{colors.BOLD}{int(hashing_speed_60)} GH/s{colors.ENDC}")
+                        logging.warning(f"")
+                        logging.warning(f"{colors.OKGREEN}{colors.UNDERLINE}Hash rate:  {colors.ENDC}")
+                        logging.warning(f"{colors.OKGREEN}60s:  {colors.FAIL}{colors.BOLD}{int(hashing_speed_60)} GH/s{colors.ENDC}")
                         logging.warning(f"{colors.OKGREEN}600s: {colors.FAIL}{colors.BOLD}{int(self.stats.hashing_speed)} GH/s{colors.ENDC}")
                         uptime = current_time - self.start_time
-                        logging.warning(f"{colors.OKCYAN}mining time {self.stats.uptime}{colors.ENDC}")
-                        logging.warning(f"{colors.OKCYAN}process uptime {int(uptime)}{colors.ENDC}")
-                        logging.warning(f"{colors.OKCYAN}{colors.UNDERLINE}________________________________________________{colors.ENDC}")
+                        logging.warning(f"{colors.OKCYAN}Mining time:    {self.stats.uptime}s{colors.ENDC}")
+                        logging.warning(f"{colors.OKCYAN}Process uptime: {int(uptime)}s{colors.ENDC}")
+                        logging.warning(f"{colors.OKCYAN}{colors.UNDERLINE}---------------------------------------------{colors.ENDC}")
 
                         hash_difficulty = shared.calculate_difficulty_from_hash(hash)
                         self.stats.best_difficulty = max(self.stats.best_difficulty, hash_difficulty)

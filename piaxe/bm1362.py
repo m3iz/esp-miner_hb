718b4460ed39cb7061aba97b830b752d52fbd8ac
Timur Mingaliev <timur@mingaliev.com>
2025-04-22 22:28:49 +0800

    Another way to set baudrate

--- piaxe/bm1362.py
+++ piaxe/bm1362.py
@@ -594,10 +594,10 @@ class BM1368(BM1362):
         self.clock_manager.do_frequency_ramp_up(frequency)
 
         # change baud
-        logging.warning('Changing baudrate to 3 000 000')
+        logging.warning('Changing baudrate to 3 125 000')
         self.send(TYPE_CMD | GROUP_ALL | CMD_WRITE, [0x00, 0x28, 0x11, 0x30, 0x02, 0x00])
         time.sleep(2)
-        self.serial_port.setBaudrate(3000000)
+        self.serial_port.baudrate = 3125000
         logging.warning(f'Baudrate changed to {self.serial_port.baudrate}')
         self.send(TYPE_CMD | GROUP_ALL | CMD_WRITE, [0x00, 0x10, 0x00, 0x00, 0x15, 0xa4])
         self.send(TYPE_CMD | GROUP_ALL | CMD_WRITE, [0x00, 0xA4, 0x90, 0x00, 0xFF, 0xFF])

7ec8590caa0f511da9e44f683498fd751d305637
Timur Mingaliev <timur@mingaliev.com>
2025-04-25 10:14:55 +0800

    Max temp = 75Cº

--- piaxe/miner.py
+++ piaxe/miner.py
@@ -353,7 +353,7 @@ class BM1366Miner:
 
 
             for i in range(0, 4):
-                if temp["temp"][i] is not None and temp["temp"][i] > 70.0:
+                if temp["temp"][i] is not None and temp["temp"][i] > 75.0:
                     logging.error("too hot, shutting down ...")
                     self.hardware.shutdown()
                     os._exit(1)
